3bf6ae8bc5f3e28266d709460c834062fc0cba4e
kesiahp18 <kesiahp18@gmail.com>
2021-02-21 21:54:14 -0500

    render single post and add comment functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,6 +41,47 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({message: 'Post not found'});
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 router.get('/login', (req, res) => {
     console.log(req.session.loggedIn)
